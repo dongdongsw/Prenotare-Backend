e96f472ccbdb4a8c01bb348080871fcdc040bc4d
DongHyeon Seo <fishman4535@gmail.com>
2026-02-09 17:45:02 +0900

    초기 설정

---
+++ src/main/java/com/sist/web/PrenotareApplication.java
@@ -0,0 +1,13 @@
+package com.sist.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrenotareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrenotareApplication.class, args);
+	}
+
+}
